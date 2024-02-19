7485828cb416dec225ced351d6387968cb08e459
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2024-02-19 16:13:30 +0100

    useCallback used

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,16 +1,26 @@
+import ProgressBar from "./ProgressBar";
 import { useEffect } from "react";
 
+const TIMER = 3000;
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  // NEVER DO IT
+  // useEffect(() => {
+  //   const fetchData = async () => {};
+  //   fetchData();
+  // }, [fetchData]);
+
   useEffect(() => {
     console.log("time started");
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMER);
     return () => {
       console.log("clear");
       clearTimeout(timer);
     };
   }, []);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
@@ -23,6 +33,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }
